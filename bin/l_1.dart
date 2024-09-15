57702619516ab9cef845c0b765d826f9e46be182
Boryslavsky <temp2003late@gmail.com>
2024-09-16 00:34:40 +0300

    1-2

---
+++ bin/l_1.dart
@@ -0,0 +1,23 @@
+void main(List<String> arguments) {
+  print('Hello World!');
+
+  10;
+  5 + 5;
+  'Dart';
+
+// English: Single-line comment. Used to add a brief comment on a single line.
+// Українською: Однорядковий коментар. Використовується для додавання короткого коментаря на одній лінії.
+
+/*
+English: Multi-line comment. Used to write comments that span multiple lines.
+Українською: Багаторядковий коментар. Використовується для написання коментарів на кількох рядках.
+*/
+
+  /// English: Documentation comment. Used to generate API documentation for the code that follows it.
+  /// Українською: Документаційний коментар. Використовується для автоматичного створення документації до коду, що йде після нього.
+
+/**
+ * English: Another form of documentation comment. Also used to document the following code and generate API docs.
+ * Українською: Ще одна форма документаційного коментаря. Також використовується для створення документації до коду та генерації API-документації.
+*/
+}
---
+++ bin/l_2.dart
@@ -0,0 +1,33 @@
+late int tweets;
+
+void main() {
+  var number = 10;
+  print(number);
+  number = 15;
+  print(number);
+
+  int like;
+  like = 5;
+  print(like);
+
+  tweets = 10;
+  print(tweets);
+
+  int? comment;
+  print(comment);
+
+  // Good comment:
+  /* 
+  personAge;
+  numberOfPeople
+  gradePointAverage
+  */
+
+  // Bad comment:
+  /*
+  a;
+  temp;
+  average
+  */
+
+}
