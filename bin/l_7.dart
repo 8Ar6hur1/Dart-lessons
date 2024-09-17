ff87a00c8da9de88d489eff800dd5b9a27d85d2e
Boryslavsky <temp2003late@gmail.com>
2024-09-17 16:23:52 +0300

    5- 7

---
+++ bin/l_7.dart
@@ -0,0 +1,22 @@
+void main() {
+  num someNumber = 5;
+  // final someInt = someNumber as int;
+  final someDouble = someNumber.toDouble();
+  print(someDouble);
+
+  // String -> int
+  var myInt = int.parse('10');
+  print(myInt);
+
+  // String - double
+  var myDouble = double.parse('5.5');
+  print(myDouble);
+
+  // int -> String
+  var s1 = 10.toString();
+  var s2 = myInt.toString();
+
+  // double -> String
+  String s3 = 3.14159.toStringAsFixed(2);
+  print(s3);
+}
