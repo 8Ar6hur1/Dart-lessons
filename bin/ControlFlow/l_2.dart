5feda5a6b4864596735c243f1c296163a7f14c6a
Boryslavsky <temp2003late@gmail.com>
2024-09-19 13:03:44 +0300

    2

---
+++ bin/ControlFlow/l_2.dart
@@ -0,0 +1,14 @@
+void main() {
+  // Switch / Case
+  String grade = '12';
+
+  switch (grade) {
+    case 'A':
+      print('Exelent grade of A');
+    case 'B':
+      print('Very good!');
+      break;
+    default:
+      print('Failed');
+  }
+}
