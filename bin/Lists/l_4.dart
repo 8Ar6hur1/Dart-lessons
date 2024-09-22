4ef65e4d04bd5474f46f1c033da3fe478fa49158
Boryslavsky <temp2003late@gmail.com>
2024-09-22 21:14:55 +0300

    4

--- bin/Lists/l_4.dart
+++ bin/Lists/l_4.dart
@@ -1,2 +1,21 @@
+void main() {
+  // spread operator (...)
+  // collection if
+  // collection for
 
-void main() {}
+  const orangeAllergt = true;
+
+  const appleAndPineApple = ['apple', 'pineapple'];
+  const orangeAndpear = [if(!orangeAllergt) 'orange', 'pear'];
+
+  final fruits = [
+    'watermelon',
+    ...appleAndPineApple,
+    ...orangeAndpear,
+  ];
+  var bigFruits = ['BANANA', for(final fruit in fruits) fruit.toUpperCase()];
+  // fruits.addAll(appleAndPineApple);
+  // fruits.addAll(orangeAndpear);
+  print(fruits);
+  print(bigFruits);
+}
